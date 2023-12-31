a10672d240f3a9d399955d2c421e654fa887bd32
Alomgir Hasan Shakib <alomgirhasanshakib@gmail.com>
2023-12-31 19:33:24 +0600

    practice for of loop

---
+++ for-of.js
@@ -0,0 +1,16 @@
+// practice for of loop 
+
+// for of loop in string 
+const name = 'Shakib'
+
+for(let letter of name){
+    console.log(letter);
+}
+
+// for of loop in an arrey 
+
+const arr = [20, 30, 40, 50, 60, 70, 80, 90]
+
+for(let sElements of arr){
+    console.log(sElements);
+}
